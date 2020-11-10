97314df4b693e75cd569fab1c414e7cd56bd5f92
Luciano <lopezz.lucio85@gmail.com>
2020-11-10 20:35:51 -0300

    removido comentario

--- core/models.py
+++ core/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class List(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete modo como vai ser deletado, entrar em doc django
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) 
     name = models.CharField(max_length=50)
 
-    def __str__(self):  # metodo de como é mostrado na tela
-        return self.name #+ " de: " + str(self.owner)
+    def __str__(self):  
+        return self.name 
 
 class Item(models.Model):
     name = models.CharField(max_length=50)
     item = models.ForeignKey(List, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
 
-    def __str__(self):  # metodo de como é mostrado na tela
+    def __str__(self): 
         return self.name
