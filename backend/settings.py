052af4f8ad08c6052029c4521b3e1ee091f2466d
Luciano <lopezz.lucio85@gmail.com>
2020-11-11 23:19:06 -0300

    settings l94

--- backend/settings.py
+++ backend/settings.py
@@ -91,5 +91,11 @@ CORS_ALLOWED_ORIGINS = [
     
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
-    "18.230.66.2"
+    "http://18.230.66.2"
 ]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
